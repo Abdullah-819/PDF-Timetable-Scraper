97219255317e22f42772e499cd12770e7b59e7e3
Abdullah Rana <ranaabdullah228.ar@gmail.com>
2026-02-06 22:24:58 +0500

    Constants creation

---
+++ core/constants.py
@@ -0,0 +1,21 @@
+DAYS = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday"
+]
+
+SLOTS = {
+    1: "8:30 - 9:55",
+    2: "9:55 - 11:20",
+    3: "11:20 - 12:45",
+    4: "1:40 - 3:05",
+    5: "3:05 - 4:30"
+}
+
+BREAK_LABELS = [
+    "Break",
+    "Fehm Ul Quran",
+    "Prayer Break"
+]
